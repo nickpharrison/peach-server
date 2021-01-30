2b6e1de17865ea512eaef6c15c88f3932b09168a
Nick Harrison <42382362+nickpharrison@users.noreply.github.com>
2021-01-30 22:54:40 +0800

    Removing explicit properties object

--- index.js
+++ index.js
@@ -17,25 +17,6 @@ PostgresPool.prototype.query = async function (...args) {
 	}
 }
 
-class PeachProperties {
-
-	constructor(propertiesPath) {
-		this.path = propertiesPath;
-		this.refreshProperties();
-	}
-
-	refreshProperties() {
-		let propertiesString;
-		try {
-			propertiesString = fs.readFileSync(this.path, 'utf8');
-		} catch(err) {
-			throw new Error('Could not read properties file during refresh');
-		}
-		this.data = JSON.parse(propertiesString);
-	}
-
-}
-
 class PeachError extends Error {
 
 	constructor(status, message, rootCause, peachCode) {
@@ -59,20 +40,32 @@ class PeachOutput {
 
 class PeachServer {
 
-	constructor({
-		peachPropertiesPath,
-	}) {
+	constructor(properties) {
 
 		this.cert = undefined;
 		this.key = undefined;
 
-		this.properties = new PeachProperties(peachPropertiesPath);
+		if (typeof properties === 'string') {
+			try {
+				properties = JSON.parse(fs.readFileSync(this.path, 'utf8'));
+			} catch(err) {
+				throw new Error('Could not read properties file during refresh');
+			}
+			properties = JSON.parse(fs.readFileSync(properties, {encoding: 'utf-8'}));
+		}
 
+		this.properties = properties;
+
+		/** @type {PostgresPool[]} */
 		this.dbPools = [];
 
-		if (Array.isArray(this.properties.data.databases)) {
+		if (this.properties == null) {
+			throw new Error('No properties file was provided');
+		}
 
-			this.properties.data.databases.forEach((dataBaseProperties) => {
+		if (Array.isArray(this.properties.databases)) {
+
+			this.properties.databases.forEach((dataBaseProperties) => {
 
 				const postgresOptions = {
 					user: dataBaseProperties.username,
@@ -100,11 +93,11 @@ class PeachServer {
 
 	getCert () {
 		if (this.cert === undefined) {
-			if (this.properties.data.security == null || this.properties.data.security.cert == null) {
+			if (this.properties.security == null || this.properties.security.cert == null) {
 				throw new Error('No cert file path was provided');
 			}
 			try {
-				this.cert = fs.readFileSync(this.properties.data.security.cert, 'utf8');
+				this.cert = fs.readFileSync(this.properties.security.cert, 'utf8');
 			} catch (err) {
 				throw new Error('Could not read the cert file, an error occurred');
 			}
@@ -114,11 +107,11 @@ class PeachServer {
 
 	getKey () {
 		if (this.key === undefined) {
-			if (this.properties.data.security == null || this.properties.data.security.key == null) {
+			if (this.properties.security == null || this.properties.security.key == null) {
 				throw new Error('No key file path was provided');
 			}
 			try {
-				this.key = fs.readFileSync(this.properties.data.security.key, 'utf8');
+				this.key = fs.readFileSync(this.properties.security.key, 'utf8');
 			} catch (err) {
 				throw new Error('Could not read the key file, an error occurred');
 			}
@@ -128,20 +121,20 @@ class PeachServer {
 
 	getRequestInfo(req) {
 
-		const acceptedHosts = Array.isArray(this.properties.data.server.acceptedhosts) ? this.properties.data.server.acceptedhosts : [];
+		const acceptedHosts = Array.isArray(this.properties.server.acceptedhosts) ? this.properties.server.acceptedhosts : [];
 
-		const proxyIsUsed = this.properties.data.server.trustedproxiessetxforwardedheaders && req.headers['x-proxy-secret'] != null && req.headers['x-proxy-secret'] === this.properties.data.server.xproxysecret;
+		const proxyIsUsed = this.properties.server.trustedproxiessetxforwardedheaders && req.headers['x-proxy-secret'] != null && req.headers['x-proxy-secret'] === this.properties.server.xproxysecret;
 
 		const proto = proxyIsUsed ? req.headers['x-forwarded-proto'] : (req.connection.encrypted ? 'https' : 'http');
 
 		const rawHost = proxyIsUsed ? req.headers['x-forwarded-host'] : req.headers['host'];
 		const host = acceptedHosts.includes(rawHost) ? rawHost : (acceptedHosts[0] || '');
 
-		const origin = (host && proto) ? `${proto}://${host}` : (typeof this.properties.data.server.defaultorigin === 'string' ? this.properties.data.server.defaultorigin : '');
+		const origin = (host && proto) ? `${proto}://${host}` : (typeof this.properties.server.defaultorigin === 'string' ? this.properties.server.defaultorigin : '');
 
 		let adjustedurl;
 		let basepath;
-		let basepaths = this.properties.data.server.basepaths;
+		let basepaths = this.properties.server.basepaths;
 		if (Array.isArray(basepaths)) {
 			for (const testbasepath of basepaths) {
 				if (req.url.startsWith(testbasepath)) {
@@ -160,7 +153,7 @@ class PeachServer {
 			throw new PeachError(500, 'Improperly configured server for server.basepaths property');
 		}
 
-		const requrl = url.parse(`${origin}${adjustedurl}`);
+		const requrl = url.parse(`${origin}${adjustedurl}`, true);
 
 		requrl.origin = requrl.protocol && requrl.host ? `${requrl.protocol}//${requrl.host}` : null;
 
@@ -172,13 +165,21 @@ class PeachServer {
 
 	}
 
+	/**
+	 * Start a new server with the requested options
+	 * @param {object} input Input object containing information to create the server
+	 * @param {(req: http.IncomingMessage, res: http.ServerResponse, requrl: url.UrlWithParsedQuery, basepath: string, ip: string, cookies: Cookies) => Promise<any>} input.requestListener The text that should be synthesised in speech
+	 * @param {number} input.incrementPort The number to increment the port number by as specified in the properties file
+	 * @param {string|null} input.authenticateMethod If a 401 or 403 error is thrown from inside the requestListener then this parameter to notify the user what authentication method they should perform
+	 * @param {object|null} input.websocketData Data relating to the websocket server to be created
+	 * @returns {void}
+	 */
 	start({
 		requestListener,
 		errorListener = null,
 		authenticateMethod = 'Basic',
-		websocketData = null,
-		websocketPingPongTimeout = 30000,
-		incrementPort = 0
+		incrementPort = 0,
+		websocketData = null
 	}) {
 
 		if (typeof requestListener !== 'function') {
@@ -311,17 +312,17 @@ class PeachServer {
 
 		};
 
-		if (this.properties.data.server == null) {
+		if (this.properties.server == null) {
 			throw new Error('Server object in properties cannot be null');
 		}
 
-		if (!Number.isInteger(this.properties.data.server.port)) {
+		if (!Number.isInteger(this.properties.server.port)) {
 			throw new Error('Server port value in properties must be an integer');
 		}
 
 		let server;
 
-		if (this.properties.data.server.usessl) {
+		if (this.properties.server.usessl) {
 
 			const https = require('https');
 
@@ -330,7 +331,7 @@ class PeachServer {
 				cert: this.getCert()
 			}, baseRequestListener);
 
-			server.listen(this.properties.data.server.port + incrementPort, '::');
+			server.listen(this.properties.server.port + incrementPort, '::');
 
 		} else {
 
@@ -338,7 +339,7 @@ class PeachServer {
 
 			server = http.createServer(baseRequestListener);
 
-			server.listen(this.properties.data.server.port + incrementPort, '::1');
+			server.listen(this.properties.server.port + incrementPort, '::1');
 
 		}
 
@@ -410,7 +411,7 @@ class PeachServer {
 						websocket.terminate();
 					}
 				}
-			}, websocketPingPongTimeout);
+			}, websocketData.pingPongTimeout || 30000);
 
 			wsServer.on('close', () => {
 				clearInterval(pingponginterval);
